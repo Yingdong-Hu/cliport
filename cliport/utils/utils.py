0942e04f687fe4a31e95d9367c67b6dac6d8b041
Alxead <1069469392@qq.com>
2023-10-21 22:00:36 +0800

    update Letters task v2

--- cliport/utils/utils.py
+++ cliport/utils/utils.py
@@ -656,8 +656,6 @@ ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
             'U', 'V', 'W', 'X', 'Y', 'Z']
 
-# ALPHABET = ['A', 'E', 'G', 'L', 'M', 'R', 'T', 'V']
-
 
 def plot(fname,  # pylint: disable=dangerous-default-value
          title,
